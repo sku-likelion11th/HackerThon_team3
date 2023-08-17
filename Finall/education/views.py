ceb9200220a391ff0832fc183f25d68a9306f1c9
nahowo <nahowo@naver.com>
2023-08-18 00:50:04 +0900

    교육/혜택 병합

--- Finall/education/views.py
+++ Finall/education/views.py
@@ -12,76 +12,49 @@ def api_education_search(request):
             config_data = json.load(json_file)
         client_educationKey = config_data["education"]["educationKey"]
         pageNo='1'
-        numOfRows='70'
-        url = 'http://apis.data.go.kr/B553701/SeominFinancialEducationContentsInfoService/getFinancialEducationContentsInfo'
-        params ={'serviceKey' : client_educationKey, 'pageNo' : pageNo, 'numOfRows' : numOfRows }
-        response = requests.get(url, params=params)
-        rescode = response.status_code
-        if (rescode == 200):
-            datas = response.text
-            root=ET.fromstring(datas)
-            datas = []
-            for data in root.findall('body/items/item'):
-                item_data={}
-                for child in data:
-                    item_data[child.tag] = child.text
-                datas.append(item_data)
-            paginator = Paginator(datas,15)
-            page = request.GET.get('page')
-            datas = paginator.get_page(page)
-            try:
-                curPage = paginator.page(page)
-            except PageNotAnInteger:
-                page=1
-                curPage = paginator.page(page)
-            leftIndex = (int(page)-3)
-            if leftIndex<1:
-                leftIndex=1
-            rightIndex = (int(page)+3)
-            if rightIndex>paginator.num_pages:
-                rightIndex=paginator.num_pages
-            custom_range=range(leftIndex, rightIndex+1)
-        return render(request, 'education/Education.html', {'datas' : datas, 'paginator': paginator, 'curPage': curPage, 'range': custom_range})
-    else:
-        return render(request, 'education/Education.html')
-
-def search(request):
-    if request.method == "GET":
-        with open('Finall/config.json') as json_file:
-            config_data = json.load(json_file)
-        client_educationKey = config_data["education"]["educationKey"]
-        pageNo='1'
         numOfRows='200'
         url = 'http://apis.data.go.kr/B553701/SeominFinancialEducationContentsInfoService/getFinancialEducationContentsInfo'
         params ={'serviceKey' : client_educationKey, 'pageNo' : pageNo, 'numOfRows' : numOfRows }
         response = requests.get(url, params=params)
         rescode = response.status_code
-        if (rescode == 200):
-            search = request.GET.get('search','')
+        if rescode == 200:
+            search = request.GET.get('search', '')
+            tag = request.GET.get('tag', '')
             datas = response.text
-            root=ET.fromstring(datas)
+            root = ET.fromstring(datas)
             datas = []
             for data in root.findall('body/items/item'):
-                item_data={}
+                item_data = {}
                 for child in data:
                     item_data[child.tag] = child.text
-                if search in item_data['edcSbjcNm'] or search in item_data['edcSbjcIntrc']:
-                    datas.append(item_data)
-            paginator = Paginator(datas,20)
+                datas.append(item_data)
+            if search or tag:
+                filtered_datas = []
+                for data in datas:
+                    search_condition = search in data['edcSbjcNm'] or search in data['edcSbjcIntrc'] or search in data['edcSmclsNm']
+                    tag_condition = tag in data['edcSmclsNm']
+                    if search_condition and tag_condition:
+                        filtered_datas.append(data)
+                datas = filtered_datas
+            paginator = Paginator(datas, 18)
             page = request.GET.get('page')
             datas = paginator.get_page(page)
             try:
                 curPage = paginator.page(page)
             except PageNotAnInteger:
-                page=1
+                page = 1
                 curPage = paginator.page(page)
-            leftIndex = (int(page)-3)
-            if leftIndex<1:
-                leftIndex=1
-            rightIndex = (int(page)+3)
-            if rightIndex>paginator.num_pages:
-                rightIndex=paginator.num_pages
-            custom_range=range(leftIndex, rightIndex+1)
-        return render(request, 'education/Education.html', {'datas' : datas, 'paginator': paginator, 'curPage': curPage, 'range': custom_range})
+
+            leftIndex = (int(page) - 3)
+            if leftIndex < 1:
+                leftIndex = 1
+
+            rightIndex = (int(page) + 3)
+            if rightIndex > paginator.num_pages:
+                rightIndex = paginator.num_pages
+
+            custom_range = range(leftIndex, rightIndex + 1)
+
+        return render(request, 'education/Education.html', {'datas': datas, 'paginator': paginator, 'curPage': curPage, 'range': custom_range, 'page':page, 'search': search, 'tag': tag})
     else:
         return render(request, 'education/Education.html')
--- Finall/service/urls.py
+++ Finall/service/urls.py
@@ -2,7 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.main_page, name = "main_page"),
     path('service/', views.api_service_search, name = "service"),
-    path('search_service/', views.search, name='search_service'),
 ]
