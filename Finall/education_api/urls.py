a52f422c032ed2da1ce238c1c07d14f2c6550289
YJKBigBo <yjg0557@naver.com>
2023-08-17 19:25:27 +0900

    finall 복구

---
+++ Finall/education_api/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+app_name = "education_api"
+urlpatterns = [
+    path('', views.api_education_search, name = "api_education_search"),
+    path('education_search/', views.search, name='education_search'),
+]
